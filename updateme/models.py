00f4229e86f660eb7a453836fef88bd5b8c58e42
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-31 09:31:19 +0300

    configure import in models and create admin class

--- updateme/models.py
+++ updateme/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Admin(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+    def save_admin(self):
+        self.save()
+
--- updateme/views.py
+++ updateme/views.py
@@ -10,3 +10,14 @@ from updateme.models import Business, NeighbourHood, User
 
 def index(request):
     businesses = Business.objects.all()
+    if 'item_name' in request.GET:
+        item_name=request.GET['item_name']
+        businesses=businesses.filter(name_icontains=item_name)
+    else:
+        businesses=Business.objects.all()
+
+    user = User.objects.all()
+    neighbourHood=NeighbourHood.objects.all()
+
+    context = {'businesses':businesses,'users':users,'neighbourhoods':neighbourhoods}
+    return render(request,'index.html',context)
