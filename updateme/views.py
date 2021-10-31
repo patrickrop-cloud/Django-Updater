c1f3fd1e8616c3368b67fda5e39be1d3b31d38fb
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-31 11:08:13 +0300

    add method to create business for a user logged in

--- updateme/views.py
+++ updateme/views.py
@@ -57,7 +57,25 @@ def loginpage(request):
                 messages.info(request, 'Username or Password missmatch')
 
     return render(request, 'registration/login.html')
+
+def logoutuser(request):
+    return redirect(reverse('login'))
+
+@login_required(login_url='login')
+def new_business(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        form = BusinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            business = form.save(commit = False)
+
+            business.save()
+    else:
+        form = BusinessForm()
     
+    return render(request, 'business.html', {"form":form})
+
 
 
 
