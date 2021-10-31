0e8e7f8ccb02993495fb75213a94b81879ce0d2d
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-31 11:14:42 +0300

    update urls file to define the correct paths

--- updateme/urls.py
+++ updateme/urls.py
@@ -7,6 +7,9 @@ from django.urls.conf import include
 urlpatterns=[
     path('',views.index,name='index'),
     path('registeruser/',views.registeruser,name='registeruser'),
-	
+    path('login/', views.loginpage, name='login'),
+    path('logout/', views.logoutuser, name='logout'),
+    path('new/business/', views.new_business, name='new_business'),
+    path('profile/', views.profile, name='profile'),
 ]
 
