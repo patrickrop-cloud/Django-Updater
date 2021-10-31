8b1b87223d7ab9d7f6157bd47864e52b8d87f05a
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-31 10:19:46 +0300

    update admin file to register our models

--- updateme/admin.py
+++ updateme/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import NeighbourHood, User, Business, Admin
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(User)
+admin.site.register(NeighbourHood)
+admin.site.register(Business)
