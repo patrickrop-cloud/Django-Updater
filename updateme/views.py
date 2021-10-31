43ca6ffbf41d021185869908b58ccbabf3571dbd
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-10-31 10:53:20 +0300

    add registration request to views to allow a user create an account

--- updateme/views.py
+++ updateme/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import request
+from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -7,7 +8,6 @@ from .forms import CreateUserForm, BusinessForm
 from updateme.models import Business, NeighbourHood, User
 
 # Create your views here.
-
 def index(request):
     businesses = Business.objects.all()
     if 'item_name' in request.GET:
@@ -17,7 +17,26 @@ def index(request):
         businesses=Business.objects.all()
 
     user = User.objects.all()
-    neighbourHood=NeighbourHood.objects.all()
+    neighbourHoods=NeighbourHood.objects.all()
 
-    context = {'businesses':businesses,'users':users,'neighbourhoods':neighbourhoods}
+    context = {'businesses':businesses,'users':user,'neighbourhoods':neighbourHoods}
     return render(request,'index.html',context)
+
+def registeruser(register):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your Account has been created succesfully!')
+
+            return redirect('login')
+        else:
+            form = CreateUserForm
+        context = {
+            'form':form,
+        }
+
+        return render(request, 'registration/register.html',context)
+
+
+
