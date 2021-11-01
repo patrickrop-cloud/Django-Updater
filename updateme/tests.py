1a050e313ca51a7917821a37eb853184b11d1085
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-11-01 14:07:43 +0300

    debug profile and add styling

--- updateme/tests.py
+++ updateme/tests.py
@@ -7,28 +7,28 @@ from .models import Business,NeighbourHood,User,Admin
 class userTestClass(TestCase):
     #setup method
     def setUp(self):
-        self.user1=User(name='fai',email="fai@gmail.com",id=12)
+        self.user0=User(name='patrick',email='patrick@gmail.com',id=1)
 
     #Testing Instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.user1,User))
+        self.assertTrue(isinstance(self.user0,User))
 
     def test_save_user(self):
-        self.user1.save_user()
-        userf=User.objects.all()
-        self.assertTrue(len(userf)>0)
+        self.user0.save_user()
+        userp=User.objects.all()
+        self.assertTrue(len(userp)>0)
 
     def test_delete_user(self):
-        self.user1.save_user()
+        self.user0.save_user()
         user_record=User.objects.all()
-        self.user1.delete_user()
+        self.user0.delete_user()
         self.assertTrue(len(user_record)==0)
 
 
 class AdminTestClass(TestCase):
     #setup method
     def setUp(self):
-        self.admin=Admin(name='fai')
+        self.admin=Admin(name='patrick')
 
     #Testing Instance
     def test_instance(self):
@@ -36,8 +36,8 @@ class AdminTestClass(TestCase):
 
     def test_save_admin(self):
         self.admin.save_admin()
-        adminf=Admin.objects.all()
-        self.assertTrue(len(adminf)>0)
+        admin_p=Admin.objects.all()
+        self.assertTrue(len(admin_p)>0)
 
     def test_delete_admin(self):
         self.admin.save_admin()
@@ -49,7 +49,7 @@ class AdminTestClass(TestCase):
 class BusinessTestClass(TestCase):
     #setup method
     def setUp(self):
-        self.business=Business(name='cereals',email="a@gmail.com")
+        self.business=Business(name='coffee',email="coffee@gmail.com")
 
     #Testing Instance
     def test_instance(self):
@@ -57,18 +57,18 @@ class BusinessTestClass(TestCase):
 
     def test_create_business(self):
         self.business.create_business()
-        bus1=Business.objects.all()
-        self.assertIsNotNone(bus1)
+        business1=Business.objects.all()
+        self.assertIsNotNone(business1)
 
-    # def test_delete_business(self):
-    #     self.business.create_business()
-    #     business_record=Business.objects.all()
-    #     self.business.delete_business()
-    #     self.assertTrue(len(business_record)==0)
+    def test_delete_business(self):
+        self.business.create_business()
+        business_record=Business.objects.all()
+        self.business.delete_business()
+        self.assertTrue(len(business_record)==0)
 
     def test_find_business(self):
         business=Business.objects.all()
-        search_term='cereals'
+        search_term='coffee'
         db_term=search_term
         if db_term !=search_term:
             return('no match')
@@ -78,14 +78,14 @@ class BusinessTestClass(TestCase):
 
     def test_update_business(self):
         business=Business.objects.all()
-        new_bus=Business.update_business(self)
-        expected_business=f'{new_bus}'
-        self.assertTrue(expected_business,'new_bus')
+        new_biz=Business.update_business(self)
+        expected_business=f'{new_biz}'
+        self.assertTrue(expected_business,'new_biz')
 
 class NeighbourhoodTestClass(TestCase):
     #setup method
     def setUp(self):
-        self.neighbourhood=NeighbourHood(name='sucess',location="kericho")
+        self.neighbourhood=NeighbourHood(name='car sales',location="kanairo")
 
     #Testing Instance
     def test_instance(self):
